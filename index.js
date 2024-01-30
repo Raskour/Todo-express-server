a0dc4be41c9dcfb8ea69da6fd4b75d48d14d037b
raskour <rasmeetkour735@gmail.com>
2024-01-30 13:12:30 +1100

    creating express server for todo app

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { Client } = require("pg");
 const dbConfig = require("./database");
 const app = express();
 
+// add body parser middleware to my express app
+app.use(express.json()); // this is for json body type middleware
 app.use(cors());
 
 // Create a PostgreSQL client
@@ -14,8 +16,8 @@ async function connectToDB() {
   try {
     await client.connect();
     console.log("Connected to the database");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000");
+    app.listen(3001, () => {
+      console.log("Server is running on port 3001");
     });
   } catch (err) {
     console.error("Error connecting to the database", err);
@@ -36,6 +38,53 @@ app.get("/todos", async (req, res) => {
   }
 });
 
+// post todo request
+app.post("/todo", async (req, res) => {
+  try {
+    const payload = req.body;
+
+    // using pg client, insert the todo into todo table in database
+    const result = await client.query(
+      "INSERT INTO todo (task, created_at, completed) VALUES ($1, $2, $3) RETURNING *",
+      [payload.task, payload.createdAt, payload.completed]
+    );
+
+    // send the todo id in the response as json
+    res.json({ id: result.rows[0].id, status: "success" });
+  } catch (err) {
+    res.json({ error: err.message }).status(400);
+  }
+});
+
+//delete todo
+app.delete("/todo/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    //This line extracts the value of the id parameter from the request URL.
+    //In Express.js, req.params is an object containing properties mapped to the
+    //named route parameters in the URL.
+    const result = await client.query("DELETE FROM todo WHERE id = $1", [id]);
+    res.json({ status: "success" }).status(200);
+  } catch (err) {
+    res.json({ error: err.message }).status(400);
+  }
+});
+
+//update a todo
+app.patch("/todo/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const payload = req.body;
+    const result = await client.query(
+      "UPDATE todo SET  completed = $1 WHERE id = $2",
+      [payload.completed, id]
+    );
+    res.json({ status: "success" }).status(200);
+  } catch (err) {
+    res.json({ error: err.message }).status(400);
+  }
+});
 app.get("/health", (req, res) => {
   return res.send("okay");
 });
